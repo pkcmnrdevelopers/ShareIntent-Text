a3fdebec46bdf46c908680f1ae59299c0ea8c424
Sabith Pkc Mnr <sabithpkcmnr@gmail.com>
2016-10-07 20:59:49 +0530

    PKCMNR Developers AS project

---
+++ app/src/main/java/com/sabithpkcmnr/sharemydata_text/MainActivity.java
@@ -0,0 +1,30 @@
+package com.sabithpkcmnr.sharemydata_text;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt=(Button)findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(Intent.ACTION_SEND);
+                myIntent.setType("text/plain");
+                String shareBody = "Your body here";
+                String shareSub = "Your Subject here";
+                myIntent.putExtra(Intent.EXTRA_SUBJECT,shareSub);
+                myIntent.putExtra(Intent.EXTRA_TEXT,shareBody);
+                startActivity(Intent.createChooser(myIntent, "Share using"));
+            }
+        });
+    }
+}
